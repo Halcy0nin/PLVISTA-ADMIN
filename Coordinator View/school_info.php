dc001b721ee63285ff1a56587216877cd665f610
Halcy0nin <155969605+Halcy0nin@users.noreply.github.com>
2024-03-07 22:01:01 +0800

    schools-page-backend-implementation

--- Coordinator View/school_info.php
+++ Coordinator View/school_info.php
@@ -1,5 +1,34 @@
-<?php //write query for all public high school info
-    $selectschoolinfo = 'SELECT * FROM high_schools ORDER BY id LIMIT 10';
+<?php 
+    // Set the default page number if it's not already set in the session
+    if (!isset($_GET["page"]))
+    {
+        $_GET["page"] = 1;
+    }
+    
+    // Set the number of items per page and the current page number
+    $itemsPerPage = 5;
+    $current_page = $_GET["page"];
+
+    // Calculate the offset based on the current page number
+    $offset = max(0, ($current_page - 1) * $itemsPerPage);
+
+    $selectschoolinfo = "SELECT *
+        FROM high_schools LIMIT $offset, $itemsPerPage";
+
+    $totalRowsQuery = "SELECT COUNT(school_name) as total_count
+        FROM high_schools";
+
+    $totalRowsResult = mysqli_query($conn, $totalRowsQuery);
+
+    // Fetch the result as an associative array
+    $totalRowsArray = mysqli_fetch_assoc($totalRowsResult);
+
+    // Access the value of total_count and store it in a variable
+    $totalCount = (int)$totalRowsArray["total_count"];
+
+    // Calculate the total number of pages
+    $totalPages = ceil($totalCount / $itemsPerPage);
+
 
     //make query and get results using the parameters (connection to be used, query to be used)
     $result = mysqli_query($conn, $selectschoolinfo);
@@ -9,6 +38,11 @@
 
     //releases results to avoid stacking up memory
     mysqli_free_result($result);
+
+    //empty strings as placeholders before form input
+$schoolname = $schoolid = $schoolcontact = $schoolcontactno = $schoolemail = $schooldivision =
+"";
+
 
 
     ?>
