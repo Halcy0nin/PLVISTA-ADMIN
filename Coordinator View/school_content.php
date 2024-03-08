977c2d758751a02e056a97d02be6b2de5bbdc63c
Halcy0nin <155969605+Halcy0nin@users.noreply.github.com>
2024-03-08 13:23:27 +0800

    school-content-backend

--- Coordinator View/school_content.php
+++ Coordinator View/school_content.php
@@ -1,6 +1,6 @@
 <?php
-include "db_conn_high_school.php";
-include "school_info.php";
+include "../Coordinator View/Processes/db_conn_high_school.php";
+include "../Coordinator View/Processes/school_info.php";
 
 ?>
 
@@ -30,9 +30,7 @@ include "school_info.php";
 
 <body>
   <!-- Bootstrap JS CDN for mobile responsiveness -->
-  <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 
     <div class = "content-container">
@@ -72,89 +70,66 @@ include "school_info.php";
         <div class = "col">
             <div class = "container d-flex justify-content-end">
 
-            <!-- add school button -->
-            <button type="button" class="btn btn-primary rounded mx-2" data-toggle="modal" data-target="#addSchool">Add School</button>
+<!-- Button for add schools -->
+<button style = "margin-left: 1020px; margin-bottom: 15px;" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#addSchool">
+        Add School
+        </button><br>
 
-
-            <!-- modal before exporting data -->
-            <div class="modal fade" id="addSchool" tabindex="-1" role="dialog" aria-labelledby="addSchoolLabel" aria-hidden="true">
-            <div class="modal-dialog" role="document">
-                <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="addSchoolLabel">Add School</h5>
-                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                    </button>
-                </div>
-
-                <form action = "add_school_process.php" method ="POST">
-                <div class="modal-body">
-
-                    <div class = "container">
-                    <h5 class = "fw-bold mb-3">School Information</h5>
-                        <div class = "row">
-
-                            <div class = "col">
-                                <div class="form-group mb-3">
-                                        <input type = "text" name ="schoolname" placeholder= "School name" value = ""></input>
-                                </div>
-
-                                <div class="form-group mb-3">
-                                    <input type = "text" name ="schoolid" placeholder= "School ID" value = ""></input>
-                                </div>
-
-                                <div class="form-group mb-3">
-                                    <input type = "text" name ="schoolcontact" placeholder= "School Contact Person" value = ""></input>
-                                </div>
-
-                                <div class="form-group mb-3">
-                                    <input type = "text" name ="schoolcontactno" placeholder= "School Contact Number" value = ""></input>
-                                </div>
-
-                                <div class="form-group">
-                                    <input type = "text" name ="schoolemail" placeholder= "School Email" value = ""></input>
-                                </div>
-                            </div>
-
-                            <div class = "col">
-                                <div class="form-group mb-3">
-                                        <select name="schooldistrict" value = "">
-                                            <option value="" disabled selected>Select School District</option>
-                                            <option>Congressional I</option>
-                                            <option>Congressional II</option>
-                                        </select>
-                                </div>
-
-                                <div class="form-group mb-3">
-                                    <select name="schooldivision">
-                                    <option value="" disabled selected>Select School Division</option>
-                                    <option>DCS-Valenzuela</option>
-                                    </select>
-                                </div>
-
-                                <div class="form-group">
-                                    <select name="schooltype">
-                                        <option value="" disabled selected>Select School Type</option>
-                                        <option> Public School </option>
-                                        <option> Private School</option>
-                                    </select>
-                                </div>
-
-                            </div>
-
-                        </div>
+        <!-- Pop-up form for adding schools -->
+        <div class="modal fade" id="addSchool" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="addSchoolLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="addSchoolLabel">Add School</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <form action = "../Coordinator View/Processes/add_school.php" method ="POST">
+            <div class="modal-body">
+                
+                    <div class="form-group mb-3">
+                        <input type = "text" name ="schoolname" placeholder= "School name" value = "<?php echo $schoolname; ?>"></input>
                     </div>
-
+                    <div class="form-group mb-3">
+                        <input type = "text" name ="schoolid" placeholder= "School ID" value = "<?php echo $schoolid; ?>"></input>
+                    </div>
+                    <div class="form-group mb-3">
+                    <label> Division </label> <br>
+                        <select name="schooldivision">
+                        <option>DCS-Valenzuela</option>
+                        </select>
+                    </div>
+                    <div class="form-group mb-3">
+                    <label> School Type </label> <br>
+                        <select name="schooltype">
+                            <option> Public School </option>
+                            <option> Private School</option>
+                        </select>
+                    </div>
+                    <div class="form-group mb-3">
+                        <input type = "text" name ="schoolcontact" placeholder= "School Contact Person" value = "<?php echo $schoolcontact; ?>"></input>
+                `   </div>
+                    <div class="form-group mb-3">
+                        <input type = "text" name ="schoolcontactno" placeholder= "School Contact Number" value = "<?php echo $schoolcontactno; ?>"></input>
+                    </div>
+                    <div class="form-group mb-3">
+                        <input type = "text" name ="schoolemail" placeholder= "School Email" value = "<?php echo $schoolemail; ?>"></input>
+                    </div>
+                    <div class="form-group mb-3">
+                    <label> School District </label> <br>
+                        <select name="schooldistrict" value = "<?php echo $schooldivision; ?>">
+                            <option>Congressional I</option>
+                            <option>Congressional II</option>
+                        </select>
+                    </div>
                 </div>
-
-        <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-            <button type="button" class="btn btn-primary">Submit</button>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="submit" name = "add_school" class="btn btn-primary">Add School</button>
             </form>
+            </div>
+            </div>
         </div>
-    </div>
-            </div>
-            </div>
+        </div>
 
 
                 <!--export data button -->
@@ -257,7 +232,7 @@ include "school_info.php";
                                 <h1 class="modal-title fs-5" id="updateschoolLabel">Edit School</h1>
                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
-                            <form action = "edit_school.php" method ="POST">
+                            <form action = "../Coordinator View/Processes/edit_school.php" method ="POST">
                             <div class="modal-body">
                                 <!-- set the following values to the last selected value in the dropdown list -->
                                 <?php
@@ -365,7 +340,7 @@ include "school_info.php";
                                             <h1 class="modal-title fs-5" id="deleteschoolLabel">Delete School</h1>
                                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                         </div>
-                                        <form action = "delete_school.php" method ="POST">
+                                        <form action = "../Coordinator View/Processes/delete_school.php" method ="POST">
                                         <div class="modal-body">
                                         <h4>Are you sure you want to delete <?php echo "<h4>" .
                                             $school["school_name"] .
@@ -386,7 +361,7 @@ include "school_info.php";
                                     </div>
                                 </div>
                                 </div>
-            <td><form action='school_inventory.php' method='GET'>
+            <td><form action='"../Coordinator View/Processes/school_inventory.php"' method='GET'>
                                 <input type='hidden' name='inventoryid' value= "<?php echo $school[
                                     "school_id"
                                 ]; ?>">
@@ -404,18 +379,18 @@ include "school_info.php";
                 <nav aria-label="Page navigation example">
                     <ul class="pagination">
                         <li class="page-item">
-                            <a class="page-link" href="dashboard.php?username=admin&password=admin&page=<?php echo max($current_page - 1, 1); ?>">Previous</a>
+                            <a class="page-link" href="school_content.php?page=<?php echo max($current_page - 1, 1); ?>">Previous</a>
                         </li>
 
                         <?php for ($i = 1;$i <= $totalPages;$i++): ?>
                             <li class="page-item">
-                                <a class="page-link" href="dashboard.php?username=admin&password=admin&page=<?php echo $i; ?>"><?php echo $i; ?></a>
+                                <a class="page-link" href="school_content.php?page=<?php echo $i; ?>"><?php echo $i; ?></a>
                             </li>
                         <?php
                 endfor; ?>
 
                         <li class="page-item">
-                            <a class="page-link" href="dashboard.php?username=admin&password=admin&page=<?php echo min($current_page + 1, $totalPages); ?>">Next</a>
+                            <a class="page-link" href="school_content.php?page=<?php echo min($current_page + 1, $totalPages); ?>">Next</a>
                         </li>
                     </ul>
              </nav>
