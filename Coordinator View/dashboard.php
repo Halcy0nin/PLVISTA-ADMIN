b515495f0cd782fe56a75cffb1847481904ac01c
JoeDelina <158024745+JoeDelina@users.noreply.github.com>
2024-03-16 21:01:40 +0800

    Add files via upload

--- Coordinator View/dashboard.php
+++ Coordinator View/dashboard.php
@@ -9,14 +9,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <title>Inventory Management System | DEPED</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> <!-- bootstrap -->
   <link rel="stylesheet" href="dashboard.css" /> <!-- css -->
   <link rel="icon" type="images/x-icon" href="sdo.png" /> <!-- icon sa tab -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> <!-- bootstrap -->
  <!-- bootstrap icons-->
  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
 
+
 <body>
+  <!-- Bootstrap JS CDN for mobile responsiveness -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  
   <!-- this is the side bar -->
   <nav class="sidebar">
     <a href="#" class="SDOlogo"><img src="sdo.png" height="80px" width="80px"></a>
@@ -52,11 +56,6 @@
   </main>
 
   <script src="dashboard.js"></script>
-
-  <!-- Bootstrap JS CDN for mobile responsiveness -->
-  <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
   <!-- loads pages indicated in data_content while retaining the sidebar-->
   <script>
