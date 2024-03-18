cfe016aaca747a4447ed0a52c820bb773584e539
JoeDelina <158024745+JoeDelina@users.noreply.github.com>
2024-03-18 16:38:40 +0800

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <title>Inventory Management System | DEPED</title>
+  
+  <link rel="stylesheet" href="dashboard.css" /> <!-- css -->
+  <link rel="icon" type="images/x-icon" href="sdo.png" /> <!-- icon sa tab -->
+ <!-- bootstrap icons-->
+ <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+</head>
+
+<body>
+
+<div class="container">
+    <div class="menu">
+  
+      <div class="main-menu">
+  
+        <div class="logo">
+        <a href="#" class="SDOlogo"><img src="sdo.png"></a>
+        </div>
+  
+        <div class="button-list">
+          <div class="item">
+          </div>
+        </div>
+
+        
+  
+      </div>
+  
+      <div class="secondary-menu" expand>
+      <div class="menu-content">
+      <ul class="menu-items">
+        <div class="menu-title">ICT Resource Management System</div>
+        <li class="item">
+          <a href="#" data-content="dashboard_content.php" class="menu-link"><i class="bi bi-bar-chart-fill"></i>Dashboard</a>
+        </li>
+        <li class="item">
+          <a href="#" data-content="resource_allocation_content.php" class="menu-link"><i class="bi bi-pie-chart-fill"></i>Resource Allocation</a>
+        </li>
+        <li class="item">
+          <a href="#" data-content="school_content.php" class="menu-link"><i class="bi bi-building-fill"></i>Schools</a>
+        </li>
+        <li class="item">
+          <a href="#" data-content="manage_users_content.php" class="menu-link"><i class="bi bi-person-vcard-fill"></i>Manage Users</a>
+        </li>
+        <li class="item">
+          <a href="#" data-content="profile_content.php" class="menu-link"><i class="bi bi-person-circle"></i>Profile</a>
+        </li>
+        <li class="item">
+          <a href="login.php"><i class="bi bi-box-arrow-in-left"></i>Log Out</a>
+        </li>
+      </ul>
+    </div>
+          </div>
+  
+        </div>
+      </div>
+  
+    </div>
+  </div>
+
+  
+    <script src="dashboard.js"></script>
+
+</body>
