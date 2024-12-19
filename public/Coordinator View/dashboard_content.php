f4bc4bc553cd74a7e89604164e5c259a2e6f4f1c
John Louis De Regla <115703434+Jilofish@users.noreply.github.com>
2024-12-19 23:58:10 +0800

    Fixed UI of cards and the most searched location chart and Average Monthly User cahrt

--- public/Coordinator View/dashboard_content.php
+++ public/Coordinator View/dashboard_content.php
@@ -38,174 +38,96 @@ include "Processes/show_announcements.php";
                 </div>
             </div>
         
-            <div class="container">
-                <div style="margin-left:3vw; margin-bottom:7vh; height: 11vh;" class="card">
-                    <div id="totalNumCard" class="card-body">
-                        <h1 class="card-title">Total Number Of Users </h1>
+            <div class="container my-4">
+                <div class="row text-center">
+                    <!-- Total Number of Users -->
+                    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+                    <div class="card h-100 d-flex flex-column">
+                        <div id="totalNumCard" class="card-body d-flex flex-column justify-content-between">
+                        <h4 class="card-title text-start">Total Number Of Users</h4>
                         <?php
-                        $totalitemcount = "SELECT COUNT(*) AS total_count FROM users WHERE is_archived = 0;
-                        ";
-                        
-                        // Make query and get results using the parameters (connection to be used, query to be used)
+                        $totalitemcount = "SELECT COUNT(*) AS total_count FROM users WHERE is_archived = 0;";
                         $result = mysqli_query($conn, $totalitemcount);
-                        
-                        // Fetch the result as an associative array
                         $row = mysqli_fetch_assoc($result);
-                        
-                        // Store the total count in a variable
                         $totalItemCount = $row['total_count'];
-                        
-                        // Release the result to avoid stacking up memory
                         mysqli_free_result($result);
-                        
                         ?>
-
-                        <h2 class="card-text"><?php echo $totalItemCount; ?></h2>
-                        <script>
-                            $('.filter-option').on('click', function(){
-                            var selectedValue = $(this).data('value');
-                            console.log("Selected value:", selectedValue);
-                            updateCardText(selectedValue);
-                        });
-                        </script>
+                        <h2 class="card-text text-end display-5"><?php echo $totalItemCount; ?></h2>
+                        </div>
                     </div>
-                </div>
-            </div>
+                    </div>
 
-                <div style="margin-left:34.4vw; margin-top:-20.7vh; height: 11vh;" class="card">
-                    <div id="totalWorkingCard" class="card-body">
-                        <h4 class="card-title">Total Visits This Month</h4>
+                    <!-- Total Visits This Month -->
+                    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+                    <div class="card h-100 d-flex flex-column">
+                        <div id="totalWorkingCard" class="card-body d-flex flex-column justify-content-between">
+                        <h4 class="card-title text-start">Total Visits This Month</h4>
                         <?php
-                        $totalVisitsMonth = 'SELECT COUNT(created_at) as total_count
-                                          FROM activity_logs WHERE created_at <= NOW()';
-                        
-                        // Make query and get results using the parameters (connection to be used, query to be used)
+                        $totalVisitsMonth = 'SELECT COUNT(created_at) as total_count FROM activity_logs WHERE created_at <= NOW()';
                         $result = mysqli_query($conn, $totalVisitsMonth);
-                        
-                        // Fetch the result as an associative array
                         $row = mysqli_fetch_assoc($result);
-                        
-                        // Store the total count in a variable
                         $totalVisitsMonth = $row['total_count'];
-                        
-                        // Release the result to avoid stacking up memory
                         mysqli_free_result($result);
-                        
                         ?>
-                        <h2 class="card-text"><?php echo $totalVisitsMonth; ?></h2>
-                        <script>
-                            $('.filter-option').on('click', function(){
-                            var selectedValue = $(this).data('value');
-                            console.log("Selected value:", selectedValue);
-                            updateCardText(selectedValue);
-                        });
-                        </script>
+                        <h2 class="card-text text-end"><?php echo $totalVisitsMonth; ?></h2>
+                        </div>
                     </div>
-                </div>
-            </div>
+                    </div>
 
-                <div style="margin-left:52.4vw; margin-top:-16.5vh; height: 11vh;" class="card">
-                    <div id="totalRepairCard"class="card-body">
-                    <h4 class="card-title">Peak Usage Time</h4>
+                    <!-- Peak Usage Time -->
+                    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+                    <div class="card h-100 d-flex flex-column">
+                        <div id="totalRepairCard" class="card-body d-flex flex-column justify-content-between">
+                        <h4 class="card-title text-start">Peak Usage Time</h4>
                         <?php
-                        // SQL Query to get peak usage hour
-                        $peakUsageTimeQuery = "
-                            SELECT 
-                                HOUR(session_start) AS peak_hour,
-                                COUNT(*) AS session_count
-                            FROM 
-                                activity_logs
-                            WHERE 
-                                session_start IS NOT NULL
-                            GROUP BY 
-                                peak_hour
-                            ORDER BY 
-                                session_count DESC
-                            LIMIT 1;
-                        ";
-                        
-                        // Execute the query
+                        $peakUsageTimeQuery = "SELECT HOUR(session_start) AS peak_hour, COUNT(*) AS session_count 
+                                                FROM activity_logs WHERE session_start IS NOT NULL 
+                                                GROUP BY peak_hour ORDER BY session_count DESC LIMIT 1;";
                         $result = mysqli_query($conn, $peakUsageTimeQuery);
-                        
-                        // Check if query execution was successful and returned a result
                         if ($result && mysqli_num_rows($result) > 0) {
-                            // Fetch the row as an associative array
                             $row = mysqli_fetch_assoc($result);
-
-                            // Retrieve the peak hour
                             $peakHour = $row['peak_hour'];
-
-                            // Convert the hour to a human-readable format (e.g., 14 -> 2 PM)
                             $humanReadableTime = date("g A", strtotime("$peakHour:00:00"));
                         } else {
-                            // Default message if no data is found or an error occurs
                             $humanReadableTime = "No Data";
                         }
-
-                        // Free result memory
                         mysqli_free_result($result);
                         ?>
-                        <!-- Display the peak usage time -->
-                        <h2 class="card-text"><?php echo $humanReadableTime; ?></h2>
-
-                        <script>
-                            // Example: JavaScript for handling additional logic
-                            $('.filter-option').on('click', function(){
-                                var selectedValue = $(this).data('value');
-                                console.log("Selected value:", selectedValue);
-                                updateCardText(selectedValue);
-                            });
-                        </script>
+                        <h2 class="card-text text-end"><?php echo $humanReadableTime; ?></h2>
+                        </div>
                     </div>
-                </div>
-            </div>
-                <div style="margin-left:70.6vw; margin-top:-16.5vh;height: 11vh;" class="card">
-                    <div id="totalCondemnedCard" class="card-body">
-                        <h4 class="card-title">Average Session Time</h4>
+                    </div>
+
+                    <!-- Average Session Time -->
+                    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
+                    <div class="card h-100 d-flex flex-column">
+                        <div id="totalCondemnedCard" class="card-body d-flex flex-column justify-content-between">
+                        <h4 class="card-title text-start">Average Session Time</h4>
                         <?php
-                         $averageSessionTime = 'SELECT 
-                         SEC_TO_TIME(AVG(TIMESTAMPDIFF(SECOND, session_start, session_end))) AS average_session_time
-                     FROM 
-                         activity_logs
-                     WHERE 
-                         session_end IS NOT NULL;
-                         ';
-                         
-                         // Make query and get results using the parameters (connection to be used, query to be used)
-                         $result = mysqli_query($conn, $averageSessionTime);
-                        
-                         // Fetch the result as an associative array
-                         $row = mysqli_fetch_assoc($result);
-                         
-                         // Store the total count in a variable
-                         $averageSessionTime = $row['average_session_time'];
-
-                         list($hours, $minutes, $seconds) = explode(':', $averageSessionTime);
-                         $humanReadableTime = "{$hours}:{$minutes}";
-                        
-                        // Release the result to avoid stacking up memory
+                        $averageSessionTime = 'SELECT SEC_TO_TIME(AVG(TIMESTAMPDIFF(SECOND, session_start, session_end))) AS average_session_time 
+                                                FROM activity_logs WHERE session_end IS NOT NULL;';
+                        $result = mysqli_query($conn, $averageSessionTime);
+                        $row = mysqli_fetch_assoc($result);
+                        $averageSessionTime = $row['average_session_time'];
+                        list($hours, $minutes, $seconds) = explode(':', $averageSessionTime);
+                        $humanReadableTime = "{$hours}:{$minutes}";
                         mysqli_free_result($result);
-                        
                         ?>
-                         <h2 class="card-text"><?php echo $humanReadableTime; ?></h2>
-                         <script>
-                            $('.filter-option').on('click', function(){
-                            var selectedValue = $(this).data('value');
-                            console.log("Selected value:", selectedValue);
-                            updateCardText(selectedValue);
-                        });
-                        </script>
+                        <h2 class="card-text text-end"><?php echo $humanReadableTime; ?></h2>
+                        </div>
+                    </div>
                     </div>
                 </div>
-                
+                </div>
+
                 <!-- CHARTS CONTAINER -->
-                <div style="display: flex; justify-content: space-between; margin-top: 4vh; padding: 0 19.5vw; gap: 2vw;">
+                <div style="display: flex; justify-content: space-between; margin-top: 4vh; padding: 0 15vw; gap: 2vw;">
 
                     <!-- MOST VISITED LOCATIONS CHART -->
                     <div style="flex: 1; height:20vw; width:45vw; background-color: #f8f9fa; border-radius: 8px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); padding: 20px;" class="cards">
                         <div id="barCard" class="cards-body">
-                            <h6 style="text-align:right; font-weight: bold; font-size: 1.5em; color: #343a40; margin-bottom:20px"><b>Most Searched Locations</b></h6>
-                            <canvas id="barChartLocations" style="width: 100%; height: 20vw;"></canvas>
+                            <h6 class="text-start fw-bold fs-4 text-dark mb-3"><b>Most Searched Locations</b></h6>
+                            <canvas id="barChartLocations" style="width: 100%; height: 20vw;" class="mb-3"></canvas>
 
                             <?php
                             $query = 'SELECT location, COUNT(location) AS count FROM activity_logs GROUP BY location HAVING COUNT(location) > 0';
@@ -255,8 +177,8 @@ include "Processes/show_announcements.php";
                     <!-- AVERAGE MONTHLY USERS CHART -->
                     <div style="flex: 1; height:20vw; width:45vw; background-color: #f8f9fa; border-radius: 8px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); padding: 20px;" class="cards">
                         <div id="barCard" class="cards-body">
-                            <h6 style="text-align:right; font-weight: bold; font-size: 1.5em; color: #343a40; margin-bottom:20px"><b>Average Monthly Users</b></h6>
-                            <canvas id="barChartUsers" style="width: 100%; height: 20vw;"></canvas>
+                            <h6 class="text-start fw-bold fs-4 text-dark mb-3"><b>Average Monthly Users</b></h6>
+                            <canvas id="barChartUsers" style="width: 100%; height: 20vw;" class="mb-4"></canvas>
 
                             <?php
                             $query = 'SELECT 
@@ -312,7 +234,7 @@ include "Processes/show_announcements.php";
                                             x: {
                                                 title: {
                                                     display: true,
-                                                    text: 'Month'
+                                                    text: ''
                                                 }
                                             }
                                         },
@@ -331,7 +253,7 @@ include "Processes/show_announcements.php";
                     </div>
                 </div>
 
-                <div style="display: flex; justify-content: space-between; margin-top: 4vh; padding: 0 19.5vw; gap: 2vw;">
+                <div style="display: flex; justify-content: space-between; margin-top: 4vh; padding: 0 19.5vw; gap: 2vw;" class="mb-3">
 
                 <!-- User Remarks Chart -->
                 <div style="height:20vw; width:45vw; background-color: #f8f9fa; border-radius: 8px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); padding: 20px;" class="cards">
